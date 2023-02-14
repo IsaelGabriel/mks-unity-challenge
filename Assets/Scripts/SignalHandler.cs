e7fe414f74475de3cf0f2702b8a56a8a80ae0b5c
Isael <isaelgsfaria@gmail.com>
2023-02-14 03:25:36 -0300

    SignalHandler added

---
+++ Assets/Scripts/SignalHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This serves as a base for most classes in the game, it handles interactions between objects in a way that they don't deppend on each other.
+/// </summary>
+public class SignalHandler : MonoBehaviour
+{
+    [SerializeField] private List<SignalHandler> listeners; // List of listeners of this object.
+    
+    public virtual void ReceiveSignal(string signal){} // Unique for each child.
+
+    protected void SendSignal(string signal) // Sends a signal to all of its listeners
+    {
+        for(int i = 0; i < listeners.Count; i++) listeners[i].ReceiveSignal(signal);
+    }
+}
