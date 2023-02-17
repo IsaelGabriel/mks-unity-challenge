9ed7484caf10627f213c15d2f3747f00fd01d226
Isael <isaelgsfaria@gmail.com>
2023-02-17 18:45:34 -0300

    Added ship life bar

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class Ship : SignalHandler
 {
-    public Sprite[] Sprites;
-    public int HP = 3;
 
+    public Sprite[] Sprites;
+
+    private int _health = 3;
     private SpriteRenderer _srenderer;
+    [SerializeField] private Transform _lifeBarContainer;
+    [SerializeField] private Transform _lifeBar;
+
 
     void Start()
     {
         _srenderer = gameObject.GetComponent<SpriteRenderer>();
-        _srenderer.sprite = Sprites[HP];
+        _srenderer.sprite = Sprites[_health];
+    }
+
+    void Update()
+    {
+        _lifeBarContainer.eulerAngles = new Vector2();
     }
 
     protected void TakeDamage()
     {
-        if(HP <= 0) return;
-        HP--;
-        _srenderer.sprite = Sprites[HP];
+        if(_health <= 0) return;
+        _health--;
+        _srenderer.sprite = Sprites[_health];
+        _lifeBar.localScale = new Vector2(_health / 3f,_lifeBar.localScale.y);
+        _lifeBar.localPosition = new Vector3(-(3-_health)/6f,_lifeBar.localPosition.y,_lifeBar.localPosition.z);
     }
+
+
 
 }
