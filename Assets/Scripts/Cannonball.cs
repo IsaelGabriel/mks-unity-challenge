2adc43bca3bc14340cd1f1509b476d87f3ac1499
Isael <isaelgsfaria@gmail.com>
2023-02-17 20:56:35 -0300

    Cannonball object and script added

---
+++ Assets/Scripts/Cannonball.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannonball : SignalHandler
+{
+
+    private Rigidbody2D _body;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
