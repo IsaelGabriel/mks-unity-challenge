ec17a55db6a79f14bf2e992778a521d076c7cabe
Isael <isaelgsfaria@gmail.com>
2023-02-17 21:03:44 -0300

    Cannonball update

--- Assets/Scripts/Cannonball.cs
+++ Assets/Scripts/Cannonball.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Cannonball : SignalHandler
 {
 
+    [HideInInspector] public string ownerType = "player";
+
+    [SerializeField] private float movementForce = 50f;
     private Rigidbody2D _body;
 
     void Start()
     {
-        
+        _body = GetComponent<Rigidbody2D>();
+        _body.AddForce(movementForce * transform.up);
     }
     
     void Update()
