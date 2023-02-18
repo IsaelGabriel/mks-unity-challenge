00767f67ad73bf03da9328338f0591ac77b8bc7a
Isael <isaelgsfaria@gmail.com>
2023-02-18 12:27:32 -0300

    Enemy ship update

--- Assets/Scripts/EnemyShip.cs
+++ Assets/Scripts/EnemyShip.cs
@@ -7,13 +7,14 @@ public class EnemyShip : Ship
     [SerializeField] protected float _movementForce = 600f;
     [SerializeField] private float _playerRadius = 5f;
     [SerializeField] private int _killPoints = 5;
+    protected Vector2 _position = new Vector2();
 
     protected override void ShipStart(){}
     protected override void ShipUpdate()
     {
-        Vector2 vPos = new Vector2(transform.position.x,transform.position.y);
-        transform.up = EnemyManager.PlayerPosition - vPos;
-        if(Vector2.Distance(vPos,EnemyManager.PlayerPosition) > _playerRadius)
+        _position = new Vector2(transform.position.x,transform.position.y);
+        transform.up = EnemyManager.PlayerPosition - _position;
+        if(Vector2.Distance(_position, EnemyManager.PlayerPosition) > _playerRadius)
         {
             _body.AddForce(transform.up * _movementForce * Time.deltaTime);
         }
