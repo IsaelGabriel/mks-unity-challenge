7b0ac9f3f99b1ae96c21cb25105f372faaf117bc
Isael <isaelgsfaria@gmail.com>
2023-02-18 12:01:02 -0300

    Enemy Manager created

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : SignalHandler
+{
+    public static Vector2 PlayerPosition = new Vector2();
+    
+    [SerializeField] private float _spawnCooldown = 5f;
+    [SerializeField] private float _updatePositionDelay = 2f;
+
+    private Transform _player;
+
+    void Start()
+    {
+        _player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        PlayerPosition = _player.position;
+    }
+
+    void Update()
+    {
+        transform.position = PlayerPosition;
+        StartCoroutine(UpdatePlayerPosition());
+    }
+
+    private IEnumerator UpdatePlayerPosition()
+    {
+        Vector2 newPos = _player.position;
+        yield return new WaitForSeconds(_updatePositionDelay);
+        PlayerPosition = newPos;
+    }
+}
