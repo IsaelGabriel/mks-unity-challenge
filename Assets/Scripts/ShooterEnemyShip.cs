1cafdeb1896c8ff1a23e60b7913399db960aeee1
Isael <isaelgsfaria@gmail.com>
2023-02-18 12:45:14 -0300

    ShooterEnemyShip code added

---
+++ Assets/Scripts/ShooterEnemyShip.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterEnemyShip : EnemyShip
+{
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private float _fireCooldown = 1f;
+    private float _fireCooldownCount = 0f;
+
+    protected override void EnemyUpdate()
+    {
+        if(_fireCooldownCount > 0f)
+        {
+            _fireCooldownCount -= Time.deltaTime;
+            return;
+        }
+        if(Vector2.Distance(_position, EnemyManager.PlayerPosition) <= _playerRadius)
+        {
+            CreateBall(_firePoint,0,false);
+            _fireCooldownCount = _fireCooldown;
+        }
+    }
+
+    protected override void Die()
+    {
+        
+    }
+
+}
