6f9bd736f6e1755553c7eefc8e6fc4294d74637b
Isael <isaelgsfaria@gmail.com>
2023-02-19 15:01:59 -0300

    ExplosionAnimator code change

--- Assets/Scripts/ExplosionAnimator.cs
+++ Assets/Scripts/ExplosionAnimator.cs
@@ -6,35 +6,31 @@ public class ExplosionAnimator : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private Sprite[] _frames;
-    [SerializeField] private float _minSize = 0.1f, _maxSize = 2f, _growthSpeed = 20f, _destructionDelay = 0.5f;
-    private float _destructionCount = 0f;
-
+    [SerializeField] private float _targetSize = 2f, _growthSpeed = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.localScale = new Vector2(_minSize,_minSize);
-        _renderer.sprite = _frames[0];
+        /*_renderer.sprite = _frames[0];*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_destructionCount > 0f)
+        /*
+        _durationCount += Time.deltaTime;
+        if(_durationCount >= _duration) Destroy(gameObject);
+        if(_durationCount >= _duration * 2/3)
         {
-            _destructionCount -= Time.deltaTime;
-            if(_destructionCount <= 0f) Destroy(gameObject);
-            return;
-        }
-
-        if(transform.localScale.x >= _maxSize)
-        {
-            _destructionCount = _destructionDelay;
             _renderer.sprite = _frames[2];
-            return;
-        }
-        transform.localScale += new Vector3(1f,1f,0f) * Time.deltaTime * _growthSpeed;
-        if(transform.localScale.x > _maxSize) transform.localScale = new Vector2(_maxSize,_maxSize);
-        if(transform.localScale.x > _maxSize/2) _renderer.sprite = _frames[1];
+        }else if(_durationCount >= _duration/3)
+        {
+            _renderer.sprite = _frames[1];
+        }*/
+
+        if(transform.localScale.x + _growthSpeed * Time.deltaTime > _targetSize) transform.localScale = new Vector2(1f,1f) * _targetSize;
+        else transform.localScale += new Vector3(1f,1f,0f) * _growthSpeed * Time.deltaTime;
+        
+        if(transform.localScale.x >= _targetSize) Destroy(gameObject);
     }
 }
