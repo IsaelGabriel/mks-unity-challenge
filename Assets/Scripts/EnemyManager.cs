0958cc18d9022a5d00c60b1b0276fd71ac208b28
Isael <isaelgsfaria@gmail.com>
2023-02-20 01:51:44 -0300

    EnemyManager code comments added

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -2,60 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Enemy Manager behaviour
+/// </summary>
 public class EnemyManager : SignalHandler
 {
-    public static Vector2 PlayerPosition = new Vector2();
+    public static Vector2 PlayerPosition = new Vector2(); // Delayed Player Position sent to all EnemyShips 
     
-    [SerializeField] private float _spawnCooldown = 5f;
-    [SerializeField] private float _updatePositionDelay = 0.5f, _minPlayerDistance = 10f;
-    [SerializeField] private Vector2 _minSpawnPoint, _maxSpawnPoint;
-    [SerializeField] private GameObject[] _prefabs;
+    // Editor variables
+    [SerializeField] private float _minPlayerDistance = 10f; // Minimal player distance when spawning enemies
+    [SerializeField] private float _updatePositionDelay = 0.5f; // PlayerPosition update delay
+    [SerializeField] private Vector2 _minSpawnPoint, _maxSpawnPoint; // Range from where enemies can be spawned
+    [SerializeField] private GameObject[] _prefabs; // Enemy prefabs
 
-    private Transform _player;
-    private float _spawnCooldownCount = 0f;
+    // Private variables
+    private Transform _player; // Player transform
+    private float _spawnCooldownCount = 0f; // Count for time between enemy spawns
 
     void Start()
     {
-        _player = GameObject.FindWithTag("Player").GetComponent<Transform>();
-        PlayerPosition = _player.position;
-        _spawnCooldown = GameManager.EnemySpawnTime;
+        _player = GameObject.FindWithTag("Player").GetComponent<Transform>(); // Find Player
+        PlayerPosition = _player.position; // Get Player's initial position
     }
 
     void Update()
     {
-        if(Time.timeScale == 0f) return;
+        if(Time.timeScale <= 0f) return; // Doesn't update if timeScale is equal or less than 0
 
-        _spawnCooldownCount -= Time.deltaTime;
-        if(_spawnCooldownCount <= 0f) Spawn();
+        _spawnCooldownCount -= Time.deltaTime; // Decrease _spawnCooldownCount by time between frames
+        if(_spawnCooldownCount <= 0f) Spawn(); // Spawn an enemy if _spawnCooldownCount has ended
 
-        StartCoroutine(UpdatePlayerPosition());
+        StartCoroutine(UpdatePlayerPosition()); // Update PlayerPosition with delay
     }
 
-    private void Spawn()
+    private void Spawn() // Spawns an enemy
     {
-        Vector3 spawnPosition = GetSpawnPosition();
-        int enemyType = Random.Range(0,_prefabs.Length);
+        Vector3 spawnPosition = GetSpawnPosition(); // Get a random spawn position for enemies
+        int enemyType = Random.Range(0,_prefabs.Length); // Get a random enemy type (_prefabs index)
 
-        var enemy = Instantiate(_prefabs[enemyType]);
+        var enemy = Instantiate(_prefabs[enemyType]); // Instantiate enemy type
 
-        enemy.transform.position = spawnPosition;
+        enemy.transform.position = spawnPosition; // Set enemy's position to randomly generated position
         enemy.transform.parent = null;
-        _spawnCooldownCount = _spawnCooldown;
+        _spawnCooldownCount = GameManager.EnemySpawnTime; // Reset cooldown count
 
     }
 
-    private Vector2 GetSpawnPosition()
+    private Vector2 GetSpawnPosition() // Get a random spawn position
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(_minSpawnPoint.x,_maxSpawnPoint.x),Random.Range(_minSpawnPoint.y,_maxSpawnPoint.y),0f);
-        if(Vector3.Distance(spawnPosition,_player.position) < _minPlayerDistance) return GetSpawnPosition();
+        Vector3 spawnPosition = new Vector3(Random.Range(_minSpawnPoint.x,_maxSpawnPoint.x),Random.Range(_minSpawnPoint.y,_maxSpawnPoint.y),0f); // Generate random position
+        if(Vector3.Distance(spawnPosition,_player.position) < _minPlayerDistance) return GetSpawnPosition(); // If spawnPosition's distance position is less than desired, run GetSpawnPosition again 
         return spawnPosition;
     }
 
 
-    private IEnumerator UpdatePlayerPosition()
+    private IEnumerator UpdatePlayerPosition() // Update PlayerPosition variable with delay
     {
-        Vector2 newPos = _player.position;
-        yield return new WaitForSeconds(_updatePositionDelay);
-        PlayerPosition = newPos;
+        Vector2 newPos = _player.position; // Get current position
+        yield return new WaitForSeconds(_updatePositionDelay); // Delay it by _updatePositionDelay's value
+        PlayerPosition = newPos; // Update PlayerPosition
     }
 }
