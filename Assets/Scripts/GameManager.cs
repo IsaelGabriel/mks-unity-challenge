dd19694bece01ab726cf400a3d9b2a206e3fd410
Isael <isaelgsfaria@gmail.com>
2023-02-19 23:41:36 -0300

    Added Settings to GameManager (Save and Set)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : SignalHandler
 {
     public static GameManager INSTANCE;
-    
+    public static float MatchTime  = 60f, EnemySpawnTime = 5f;
+
+
     private int _score = 0;
     private TextMeshProUGUI _scoreTextObject;
 
@@ -39,6 +42,19 @@ public class GameManager : SignalHandler
             default:
                 if(signal.Contains("ChangeScene:"))
                 {
+                    if(signal.Split()[1] == "RevertSettings")
+                    {
+                        PlayerPrefs.SetFloat("MatchTime", 60f);
+                        PlayerPrefs.SetFloat("SpawnTime", 5f);
+                        SceneManager.LoadScene("MainMenu");
+                        return;
+                    }
+                    if(SceneManager.GetActiveScene().name == "Settings")
+                    {
+                        PlayerPrefs.SetFloat("MatchTime", GameObject.Find("Canvas/Match Time/Slider").GetComponent<Slider>().value);
+                        PlayerPrefs.SetFloat("SpawnTime", GameObject.Find("Canvas/Enemy Spawn Time/Slider").GetComponent<Slider>().value);
+                    }
+
                     SceneManager.LoadScene(signal.Split()[1]);
                 }
 
@@ -48,10 +64,17 @@ public class GameManager : SignalHandler
 
     protected void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        MatchTime = PlayerPrefs.GetFloat("MatchTime", MatchTime);
+        EnemySpawnTime = PlayerPrefs.GetFloat("SpawnTime", EnemySpawnTime);
+
         if(scene.name == "SampleScene")
         {
-            _scoreTextObject = GameObject.Find("Canvas").transform.Find("Score Text").GetComponent<TextMeshProUGUI>();
+            _scoreTextObject = GameObject.Find("Canvas/Score Text").GetComponent<TextMeshProUGUI>();
             _scoreTextObject.text = $"Pontos: {_score}";
+        }else if(scene.name == "Settings")
+        {
+            GameObject.Find("Canvas/Match Time/Slider").GetComponent<Slider>().value = PlayerPrefs.GetFloat("MatchTime", MatchTime);
+            GameObject.Find("Canvas/Enemy Spawn Time/Slider").GetComponent<Slider>().value = PlayerPrefs.GetFloat("SpawnTime", EnemySpawnTime);
         }
 
     }
