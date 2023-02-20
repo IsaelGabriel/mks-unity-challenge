ee6d2c86044b1505b22f63c582e9ba248179c5e7
Isael <isaelgsfaria@gmail.com>
2023-02-20 01:18:22 -0300

    ChaserShip commented

--- Assets/Scripts/ChaserShip.cs
+++ Assets/Scripts/ChaserShip.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Chaser Ship behaviour
+/// <summary>
 public class ChaserShip : EnemyShip
 {
-    protected override void EnemyUpdate()
-    {
-
-    }
+    protected override void EnemyUpdate() {} // Custom Update (Inherited from EnemyShip)
 
     void OnCollisionEnter2D(Collision2D c)
     {
-        if(c.gameObject.GetComponent<Ship>())
+        if(c.gameObject.GetComponent<Ship>()) // If it has collided with another ship
         {
-            _health = 0;
-            c.gameObject.GetComponent<Ship>().TakeDamage();
-            CreateObj(_greatExplosionPrefab,transform,0f,true);
-            Die();
+            _health = 0; // Set health to 0
+            c.gameObject.GetComponent<Ship>().TakeDamage(); // Deal damage to collided ship
+            CreateObj(_greatExplosionPrefab,transform,0f,true); // Explode
+            Die(); // Call Die method inherited from EnemyShip
         }
     }
 }
