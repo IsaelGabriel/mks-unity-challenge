e6484a30bb51c7ec32c283942f3ac3070b999abb
Isael <isaelgsfaria@gmail.com>
2023-02-20 01:38:25 -0300

    ExplosionAnimator code comments added

--- Assets/Scripts/ExplosionAnimator.cs
+++ Assets/Scripts/ExplosionAnimator.cs
@@ -2,34 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Explosion simple animation code
+/// <summary>
 public class ExplosionAnimator : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer _renderer;
-    [SerializeField] private float _targetSize = 2f, _growthSpeed = 2f;
+    [SerializeField] private float _targetSize = 2f, _growthSpeed = 2f; // Variables for explosions desired size and growth speed
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        /*_renderer.sprite = _frames[0];*/
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        /*
-        _durationCount += Time.deltaTime;
-        if(_durationCount >= _duration) Destroy(gameObject);
-        if(_durationCount >= _duration * 2/3)
-        {
-            _renderer.sprite = _frames[2];
-        }else if(_durationCount >= _duration/3)
-        {
-            _renderer.sprite = _frames[1];
-        }*/
-
-        if(transform.localScale.x + _growthSpeed * Time.deltaTime > _targetSize) transform.localScale = new Vector2(1f,1f) * _targetSize;
-        else transform.localScale += new Vector3(1f,1f,0f) * _growthSpeed * Time.deltaTime;
+        if(transform.localScale.x + _growthSpeed * Time.deltaTime > _targetSize) transform.localScale = new Vector2(1f,1f) * _targetSize; // If size is going to become bigger than desired size, make it desired size
+        else transform.localScale += new Vector3(1f,1f,0f) * _growthSpeed * Time.deltaTime; // Else, grow
         
-        if(transform.localScale.x >= _targetSize) Destroy(gameObject);
+        if(transform.localScale.x >= _targetSize) Destroy(gameObject); // If size equal or bigger than desired size, destroy object
     }
 }
