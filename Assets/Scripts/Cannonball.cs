137573028533c2be667ca8fae2960daf34767303
Isael <isaelgsfaria@gmail.com>
2023-02-20 01:31:40 -0300

    Cannonball code comments added

--- Assets/Scripts/Cannonball.cs
+++ Assets/Scripts/Cannonball.cs
@@ -2,25 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cannonball : SignalHandler
+/// <summary>
+/// Cannonball behaviour
+/// <summary>
+public class Cannonball : MonoBehaviour
 {
+    [HideInInspector] public bool OwnedByPlayer = false; // Made to be checked when hitting a ship, define by parent ship
 
-    [HideInInspector] public bool OwnedByPlayer = false;
-
-    [SerializeField] private float _movementForce = 200f;
-    private Rigidbody2D _body;
-    private float _destroyCount = 5f;
-
+    [SerializeField] private float _movementForce = 200f; // Force added when created
+    private Rigidbody2D _body; // Object's Rigidbody2D
+    private float _destroyCount = 5f; // Countdown for self destruction in case it doesn't hit anything
 
     void Start()
     {
-        _body = GetComponent<Rigidbody2D>();
-        _body.AddForce(_movementForce * transform.up);
+        _body = GetComponent<Rigidbody2D>(); // Get object's Rigidbody2D
+        _body.AddForce(_movementForce * transform.up); // Add force forward
     }
 
     void Update()
     {
-        _destroyCount -= Time.deltaTime;
-        if(_destroyCount <= 0f) Destroy(gameObject);    
+        _destroyCount -= Time.deltaTime; // Decrease countdown
+        if(_destroyCount <= 0f) Destroy(gameObject); // Destroy object in case it stays alive for more than 5 seconds
     }
 }
