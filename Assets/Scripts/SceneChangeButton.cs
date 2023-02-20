ebd9ba45e841377eca46414f33bc12538f31ca81
Isael <isaelgsfaria@gmail.com>
2023-02-20 01:58:02 -0300

    SceneChangeButton code comments added

--- Assets/Scripts/SceneChangeButton.cs
+++ Assets/Scripts/SceneChangeButton.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///  buttons that send a "ChangeScene" signal to the current GameManager
+/// </summary>
 public class SceneChangeButton : SignalHandler
 {
-
     void Start()
     {
-        AddListener(GameManager.INSTANCE);
+        AddListener(GameManager.INSTANCE); // Add the current GameManager to Listener list
     }
 
-    public void ButtonClicked(string sceneID)
+    public void ButtonClicked(string sceneID) // Call when the button is clicked, sends "ChangeScene" signal
     {
-        SendSignal($"ChangeScene: {sceneID}");
+        SendSignal($"ChangeScene: {sceneID}"); // Send "ChangeScene" signal to GameManager with desired scene's name
     }
 
 }
